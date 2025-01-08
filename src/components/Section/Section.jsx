840c14e57bf5482112b69a4ebac4ece4671fcfe0
RaulVillarraso <raulvillarraso@gmail.com>
2025-01-08 12:48:23 +0000

    section card created

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,12 @@
+import './Section.css'
+
+function Section() {
+  return (
+    <div className="sectionCard">
+      <h1>Section</h1>
+      <p>Subsection</p>
+    </div>
+  )
+}
+
+export default Section
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import './Home.css'
+import Section from '../../components/Section/Section'
 
 function Home() {
   return (
-    <div className="mainPhoto"></div>
+    /* <div className="mainPhoto"></div> */
+    <Section />
   )
 }
 
